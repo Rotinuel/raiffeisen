3b3e1436513bf99d3588ee2e3c7207c0d5c42869
Xalph-Luthor <emmanuelisagenius@gmail.com>
2021-05-23 13:38:29 +0100

    terrible

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,15 @@ import React, { Component } from 'react';
 import Navbar from '../components/Layouts/Navbar';
 import MainBanner from '../components/Home/MainBanner';
 import OTPBox from '../components/Layouts/otp';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ReactDOM from 'react-dom';
+
 
 class Index extends Component {
     render() {
         return (
             <>
-            <Router>
-            <Navbar />
-                <Switch>
-                    <Route exact path="/">
-                        <MainBanner />
-                    </Route>
-                    <Route path="/OTPBox">
-                        <OTPBox/>
-                    </Route>
-                </Switch>
-            </Router>
-                
-
-            
+                <Navbar />
+                <MainBanner />
+                <OTPBox/>           
             </>
         );
     }
